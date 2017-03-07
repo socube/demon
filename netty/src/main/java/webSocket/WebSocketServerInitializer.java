7ed01d3b10ad38b37c56f305c1f5f68e39c078e6
qingyang <qingyang@aimeizhuyi.com>
2017-03-07 17:10:46 +0800

    添加netty webSocket

---
+++ netty/src/main/java/webSocket/WebSocketServerInitializer.java
@@ -0,0 +1,25 @@
+package webSocket;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
+/**
+ * @Description
+ * @Author xuedong.wang
+ * @Date 17/3/7.
+ */
+public class WebSocketServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new HttpObjectAggregator(65536));
+        pipeline.addLast(new WebSocketServerCompressionHandler());
+        pipeline.addLast(new WebSocketServerHandler());
+    }
+}
