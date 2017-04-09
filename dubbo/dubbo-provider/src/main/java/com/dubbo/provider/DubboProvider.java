1a1e1b18f96f60dbc534fe4355656d43897808b3
wangxuedong <wangxuedong@xin.com>
2017-04-09 18:16:26 +0800

    添加dubbo 提供者 和消费者

--- dubbo/dubbo-provider/src/main/java/com/dubbo/provider/DubboProvider.java
+++ dubbo/dubbo-provider/src/main/java/com/dubbo/provider/DubboProvider.java
@@ -1,7 +1,13 @@
 package com.dubbo.provider;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ProtocolConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import com.alibaba.dubbo.config.ServiceConfig;
 import com.dubbo.api.UserService;
 import com.dubbo.api.entity.User;
+import com.dubbo.provider.impl.UserServiceImpl;
+
 
 import java.util.List;
 
@@ -10,7 +16,42 @@ import java.util.List;
  * @Author xuedong.wang
  * @Date 17/4/8.
  */
-public class DubboProvider{
+public class DubboProvider {
 
 
+    public static void main(String[] args) {
+
+        // 服务实现
+        UserService xxxService = new UserServiceImpl();
+
+        // 当前应用配置
+        ApplicationConfig application = new ApplicationConfig();
+        application.setName("dubbo-provider");
+
+        // 连接注册中心配置
+        RegistryConfig registry = new RegistryConfig();
+        registry.setAddress("192.168.201.219:2181,192.168.201.220:2181,192.168.201.218:2181");
+        //registry.setUsername("aaa");
+        //registry.setPassword("bbb");
+
+        // 服务提供者协议配置
+        ProtocolConfig protocol = new ProtocolConfig();
+        protocol.setName("dubbo");
+        protocol.setPort(20800);
+        //protocol.setThreads(200);
+
+        // 注意：ServiceConfig为重对象，内部封装了与注册中心的连接，以及开启服务端口
+
+        // 服务提供者暴露服务配置
+        ServiceConfig<UserService> service = new ServiceConfig<UserService>(); // 此实例很重，封装了与注册中心的连接，请自行缓存，否则可能造成内存和连接泄漏
+        service.setApplication(application);
+        service.setRegistry(registry); // 多个注册中心可以用setRegistries()
+        service.setProtocol(protocol); // 多个协议可以用setProtocols()
+        service.setInterface(UserService.class);
+        service.setRef(xxxService);
+        service.setVersion("1.0.0");
+
+        // 暴露及注册服务
+        service.export();
+    }
 }
