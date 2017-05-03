eddee5b830a49be1a7d38ac78edc4c7542642fa4
wangxuedong <wangxuedong@xin.com>
2017-05-03 11:49:38 +0800

    添加netty 服务端和客户端

---
+++ netty/src/main/java/simple/SimpleChatClientHandler.java
@@ -0,0 +1,17 @@
+package simple;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * 客户端 channel
+ *
+ * @author  xuedong.wang
+ * @date 2017-5-3
+ */
+public class SimpleChatClientHandler extends  SimpleChannelInboundHandler<String> {
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, String s) throws Exception {
+		System.out.println(s);
+	}
+}
