dd8647ca468d9b7ca15628895a659795044d382f
wangxuedong <wangxuedong@xin.com>
2017-05-08 18:15:01 +0800

    添加服务端自定义协议操作

---
+++ netty/src/main/java/protocol/ProtocolServerHandler.java
@@ -0,0 +1,31 @@
+package protocol;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @Description
+ * @Author xuedong.wang
+ * @Date 17/5/8.
+ */
+public class ProtocolServerHandler extends SimpleChannelInboundHandler<Object> {
+ 
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, Object obj)
+			throws Exception {
+        Channel incoming = ctx.channel();
+		
+		if(obj instanceof ProtocolMsg) {
+			ProtocolMsg msg = (ProtocolMsg)obj;
+			System.out.println("Client->Server:"+incoming.remoteAddress()+msg.getBody());
+			incoming.write(obj);
+		}
+	}
+	
+	@Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+}
