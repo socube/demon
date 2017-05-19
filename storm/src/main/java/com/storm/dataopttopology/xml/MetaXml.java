d7ba5fea2abbfa9ba52b109ba0ecb3dd005a15c9
wangxuedong <wangxuedong@xin.com>
2017-05-19 14:42:24 +0800

    添加xml 和 util

---
+++ storm/src/main/java/com/storm/dataopttopology/xml/MetaXml.java
@@ -0,0 +1,55 @@
+package com.storm.dataopttopology.xml;
+
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import com.storm.dataopttopology.util.MacroDef;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+/**
+ * @Description 消费者Metaq接口的配置xml读取接口
+ * @Author xuedong.wang
+ * @Date 17/5/19.
+ */
+public class MetaXml {
+
+    //xml路径
+    private static String fd;
+    //MetaBolt参数
+    //!--MetaQ消息队列--
+    public static String MetaTopic;
+    //!--MetaQ服务地址--
+    public static String MetaZkConnect;
+    //!--MetaQ服务路径--
+    public static String MetaZkRoot;
+
+
+    @SuppressWarnings("static-access")
+    public MetaXml(String str){
+        this.fd = str;
+    }
+
+    @SuppressWarnings("static-access")
+    public void read(){
+        try {
+            File file = new File(this.fd);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+
+            NodeList nl = doc.getElementsByTagName(MacroDef.Parameter);
+
+            Element e = (Element)nl.item(0);
+
+            MetaTopic = e.getElementsByTagName(MacroDef.MetaTopic).item(0).getFirstChild().getNodeValue();
+            MetaZkConnect = e.getElementsByTagName(MacroDef.MetaZkConnect).item(0).getFirstChild().getNodeValue();
+            MetaZkRoot = e.getElementsByTagName(MacroDef.MetaZkRoot).item(0).getFirstChild().getNodeValue();
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
