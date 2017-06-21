d5201c292b130f587492ae264fb8e9d086d6c050
wangxuedong <wangxuedong@xin.com>
2017-06-21 17:18:13 +0800

    add thrift zk demon

--- web/src/main/java/com/collection/MapDemon.java
+++ web/src/main/java/com/collection/MapDemon.java
@@ -2,6 +2,7 @@ package com.collection;
 
 import junit.framework.TestCase;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -11,22 +12,67 @@ import java.util.TreeMap;
  * @Author xuedong.wang
  * @Date 17/6/8.
  */
-public class MapDemon extends TestCase{
+public class MapDemon extends TestCase {
 
 
+    public void testMutableKey() {
+        class MyKey {
+            Integer i;
 
-    public void testMapiterotor(){
+            public void setI(Integer i) {
+                this.i = i;
+            }
 
-        Map<String,Object> map = new HashMap<String,Object>();
+            public MyKey(Integer i) {
+                this.i = i;
+            }
+
+            @Override
+            public int hashCode() {
+                // 如果返回1
+                // return 1
+                return i;
+            }
+
+            // object作为key存map里，必须实现equals方法
+            @Override
+            public boolean equals(Object obj) {
+                if (obj instanceof MyKey) {
+                    return i.equals(((MyKey) obj).i);
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        // 我机器配置不高，25000的话正常情况27毫秒，可以用2500万试试，如果hashCode()方法返回1的话，250万就卡死
+        Map<MyKey, String> map = new HashMap<MyKey, String>();
+        Date begin = new Date();
+        for (int i = 0; i < 200000; i++) {
+            map.put(new MyKey(i), "test " + i);
+        }
+
+        Date end = new Date();
+        System.out.println("时间(ms) " + (end.getTime() - begin.getTime()));
+    }
 
 
-        map.put("name","wang");
-        map.put("age",12);
+    public void testSys() {
+        System.out.println(1 << 30);
+    }
+
+    public void testMapiterotor() {
+
+        Map<String, Object> map = new HashMap<String, Object>();
+
+
+        map.put("name", "wang");
+        map.put("age", 12);
 
     }
 
-    public void testSortTreeMap(){
-        Map<String,Object> tmap =new  TreeMap<String,Object>();
+    public void testSortTreeMap() {
+        Map<String, Object> tmap = new TreeMap<String, Object>();
 
         tmap.put("yuwen", "语文");
         tmap.put("english", "英语");
@@ -38,9 +84,9 @@ public class MapDemon extends TestCase{
         tmap.put("hua", "化学");
 
         int count = 0;
-        for (Object str : tmap.values()){
-            count ++;
-            System.out.println("第 "+count+"="+str);
+        for (Object str : tmap.values()) {
+            count++;
+            System.out.println("第 " + count + "=" + str);
         }
     }
 }
