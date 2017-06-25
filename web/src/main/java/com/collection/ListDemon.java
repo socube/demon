748aab923ad8c9038c8a6564a6746eeb9d651737
wangxuedong <wangxuedong@xin.com>
2017-06-25 20:50:32 +0800

    add thrift zk demon

--- web/src/main/java/com/collection/ListDemon.java
+++ web/src/main/java/com/collection/ListDemon.java
@@ -1,9 +1,11 @@
 package com.collection;
 
+import junit.framework.TestCase;
+
 /**
  * @Description
  * @Author xuedong.wang
  * @Date 17/6/8.
  */
-public class ListDemon {
+public class ListDemon extends TestCase{
 }
