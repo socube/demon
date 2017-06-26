18be116d10a490fcfee03e8cc2bb32050b75d6e7
wangxuedong <wangxuedong@xin.com>
2017-06-26 18:00:24 +0800

    add thrift zk demon

--- web/src/main/java/com/sign/TestSign.java
+++ web/src/main/java/com/sign/TestSign.java
@@ -14,11 +14,14 @@ public class TestSign extends TestCase {
 
     public void testSignDemon() {
 
+        //publishid=1676549&session=70f638907a795f5e1c66434f5574e79e&username=gues/tt&version=7.0&tp=th ok&so=g*o
         JSONObject obj = new JSONObject();
-        obj.put("name","wangxuedong");
-        obj.put("sex",1);
-        obj.put("age",20);
-
+        obj.put("publishid",1676549);
+        obj.put("session","70f638907a795f5e1c66434f5574e79e");
+        obj.put("username","gues/tt");
+        obj.put("version","7.0");
+        obj.put("tp","th ok");
+        obj.put("so","g*o");
         String str = JSON.toJSONString(obj);
 
 
@@ -28,7 +31,7 @@ public class TestSign extends TestCase {
 
     private static String getSign(String params){
         StringBuilder sb = new StringBuilder();
-       // MD5Util.getMD5Str(sb.toString()).toLowerCase();
+        //MD5Util.getMD5Str(sb.toString()).toLowerCase();
         return null;
     }
 }
