cc8ea1643fe60a8cb51a3f054f8441a8f8637c84
wangxuedong <wangxuedong@xin.com>
2017-06-27 18:13:36 +0800

    add thrift zk demon

---
+++ web/src/main/java/com/Idea.java
@@ -0,0 +1,17 @@
+package com;
+
+import junit.framework.TestCase;
+
+/**
+ * @Description
+ * @Author xuedong.wang
+ * @Date 17/6/27.
+ */
+public class Idea extends TestCase {
+
+
+    public void testCase() {
+
+    }
+
+}
