d33008e94e42cfaf68fdedf0bb34ad0ab578942b
wangxuedong <wangxuedong@xin.com>
2017-07-15 22:25:35 +0800

    add rocketmq

---
+++ mq/src/main/java/com/mq/rocketmq/RocketmqUtil.java
@@ -0,0 +1,26 @@
+package com.mq.rocketmq;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+/**
+ * @Description
+ * @Author xuedong.wang
+ * @Date 17/3/3.
+ */
+public class RocketmqUtil {
+
+
+    public final static String NAMESERVER_PORT = "9876";
+
+    public final static String NAMESERVER_IP = "192.168.201.229";
+
+    public final static String NAMESERVERADDR = NAMESERVER_IP + ":" + NAMESERVER_PORT;
+
+    public final static String BROKEN_IP = NAMESERVER_IP;
+
+    public final static String BROKEN_PORT = "10911";
+
+    public final static String BROKENADDR = BROKEN_IP + ":" + BROKEN_PORT;
+
+
+}
