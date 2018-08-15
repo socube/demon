c495d1ab01417ddfb0d8b003901221cc7ba131ff
wangxuedong <wangxuedong@xin.com>
2018-08-15 16:34:12 +0800

    add spring websocket

---
+++ web/src/main/java/bytebuddy/Dog.java
@@ -0,0 +1,12 @@
+package bytebuddy;
+
+/**
+ * @Description
+ * @Author xuedong.wang
+ * @Date 18/8/15.
+ */
+public class Dog {
+    public int hello() {
+        return 0;
+    }
+}
---
+++ web/src/main/java/bytebuddy/IAnimal.java
@@ -0,0 +1,9 @@
+package bytebuddy;
+
+/**
+ * @Description
+ * @Author xuedong.wang
+ * @Date 18/8/15.
+ */
+public interface IAnimal {
+}
